005186bcb86525b365b8d95a7f02802b1adc4c5e
Pita's Life <74688531+PitaLife@users.noreply.github.com>
2021-10-28 11:37:36 +0700

    Add files via upload

---
+++ scripts/questions.js
@@ -0,0 +1,168 @@
+let questions_by_category = [
+  {
+    category: "Robots",
+    10: {
+      question_text:
+        'SPOILER ALERT: HIS DYING WORDS IN THE ANIMATED TRANSFORMERS MOVIE ARE "TIL ALL ARE ONE"',
+      answer_text: "Who is Optimus Prime?"
+    },
+    20: {
+      question_text:
+        "THIS ROBOT BOUNTY HUNTER WAS HIRED TO FIND THE MILLENNIUM FALCON IN THE EMPIRE STRIKES BACK",
+      answer_text: "Who is IG-88 or 4-LOM?"
+    },
+    30: {
+      question_text:
+        "ROBOTS B.O.B. AND V.I.N.C.E.N.T. PROVIDED SOME COMIC RELIEF IN THIS DARK 1979 DISNEY SCI-FI FILM",
+      answer_text: "What is The Black Hole?"
+    },
+    40: {
+      question_text:
+        "CHRIS JERICHO HOSTED THIS AUTOMATON FIGHTING COMPETITION ON SYFY IN EARLY 2013",
+      answer_text: "What is Robot Combat League?"
+    },
+    50: {
+      question_text:
+        "HE'S THE ROBOT WHO GETS ELECTROCUTED AND COMES TO LIFE IN 1986'S SHORT CIRCUIT",
+      answer_text: "Who is Johnny 5/Number 5?"
+    }
+  },
+  {
+    category: "Nicknames",
+    10: {
+      question_text:
+        "MINECRAFT CREATOR MARKUS PERSSON IS BETTER KNOWN BY THIS MONOSYLLABIC NICKNAME",
+      answer_text: "What is Notch?"
+    },
+    20: {
+      question_text:
+        "IT'S THE ALTER EGO OF PENNY ARCADE'S MIKE KRAHULIK - AND THE NAME OF MIKE'S SON",
+      answer_text: "What is Gabe?"
+    },
+    30: {
+      question_text:
+        "ESPORTSCASTER AND MACHINIMA PERSONALITY RYAN WYATT IS BEST KNOWN BY THIS SHORT NICKNAME",
+      answer_text: "What is Fwiz?"
+    },
+    40: {
+      question_text:
+        'IN ADDITION TO "CLIFFYB", CLIFF BLESZINSKI IS SOMETIMES KNOWN BY THIS LARGE NICKNAME, BRO',
+      answer_text: "What is Dude Huge?"
+    },
+    50: {
+      question_text:
+        'LARRY HRYB BORROWED HIS GAMERTAG, "MAJOR NELSON", FROM THIS 1960S SITCOM',
+      answer_text: "What is I Dream of Jeannie?"
+    }
+  },
+  {
+    category: "Famous first words",
+    10: {
+      question_text: '"BITE MY SHINY METAL ASS"',
+      answer_text: "Who is Bender?"
+    },
+    20: {
+      question_text:
+        '"WHY, YOU SLIMY, DOUBLE-CROSSING, NO-GOOD SWINDLER. YOU GOT A LOT OF GUTS COMING HERE, AFTER WHAT YOU PULLED"',
+      answer_text: "Who is Lando Calrissian?"
+    },
+    30: {
+      question_text: '"LISTEN TO MY STORY. THIS MAY BE OUR LAST CHANCE."',
+      answer_text: "Who is Tidus?"
+    },
+    40: {
+      question_text:
+        '"FARMBOY, POLISH MY HORSE\'S SADDLE. I WANT TO SEE MY FACE SHINING IN IT BY MORNING"',
+      answer_text: "Who is Princess Buttercup?"
+    },
+    50: {
+      question_text:
+        '"THIS IS IT. THIS IS WHERE FORRESTAL CASHED IN HIS CHIPS"',
+      answer_text: "Who is Indiana Jones?"
+    }
+  },
+  {
+    category: "Going 'dark'",
+    10: {
+      question_text:
+        "THIS UNFORGIVING ACTION/RPG FROM FROM SOFTWARE TAKES PLACE IN THE KINGDOM OF LORDRAN",
+      answer_text: "What is Dark Souls?"
+    },
+    20: {
+      question_text:
+        "TIM BURTON AND JOHNNY DEPP RESURRECTED THIS LATE-60S VAMPIRE SOAP OPERA AS A COMEDY FILM IN 2012",
+      answer_text: "What is Dark Shadows?"
+    },
+    30: {
+      question_text:
+        "THIS CLASSIC FRANK MILLER GRAPHIC NOVEL FEATURES A BITTER, AGING BATMAN AND A FEMALE ROBIN",
+      answer_text: "What is The Dark Knight Returns?"
+    },
+    40: {
+      question_text:
+        "GELFLINGS KIRA AND JEN ESCAPE THE SKEKSIS IN THIS 1982 JIM HENSON CULT FANTASY FAVORITE",
+      answer_text: "What is The Dark Crystal?"
+    },
+    50: {
+      question_text:
+        "THE SPIDER-MAN STAGE MUSICAL, NOW PLAYING ON BROADWAY, FEATURES THIS UNUSUAL SUBTITLE",
+      answer_text: "What is Turn Off the Dark?"
+    }
+  },
+  {
+    category: "Fictional companies",
+    10: {
+      question_text:
+        "YOU CAN BLAME THIS FICTIONAL COMPANY FOR RESIDENT EVIL'S T-VIRUS",
+      answer_text: "What is Umbrella?"
+    },
+    20: {
+      question_text:
+        '"MORE HUMAN THAN HUMAN" IS THE MOTTO OF THIS REPLICANT MANUFACTURER FROM BLADE RUNNER',
+      answer_text: "What is Tyrell Corporation?"
+    },
+    30: {
+      question_text:
+        "MILES DYSON CREATED NEURAL-NET PROCESSORS FOR THIS COMPANY, WHICH ULTIMATELY BUILDS SKYNET",
+      answer_text: "What is Cyberdyne Systems?"
+    },
+    40: {
+      question_text:
+        "THEY OWN ROBOCOP, ED-209, AND THE ENTIRE DETROIT POLICE FORCE",
+      answer_text: "What is Omni Consumer Products/OCP?"
+    },
+    50: {
+      question_text:
+        "IN TRON, KEVIN FLYNN HACKS HIS WAY INTO THE NETWORK OF THIS FICTIONAL GAME PUBLISHER",
+      answer_text: "What is Encom?"
+    }
+  },
+  {
+    category: "Painfully obvious",
+    10: {
+      question_text:
+        "SHE'S THE PRINCESS IN THE CLASSIC RPG THE LEGEND OF ZELDA",
+      answer_text: "Who is Zelda?"
+    },
+    20: {
+      question_text:
+        "BUNGIE'S WORKING TITLE FOR MARATHON WAS MARATHON, BUT ONI'S WORKING TITLE WAS THIS",
+      answer_text: "What is Oni?"
+    },
+    30: {
+      question_text:
+        "THIS TWO-BUTTON FIGHTING GAME STARS TWO GUYS NAMED KICK AND DIVE",
+      answer_text: "What is Divekick?"
+    },
+    40: {
+      question_text:
+        "BRODERBUND'S CASTLE WOLFENSTEIN WAS AN INSPIRATION FOR THIS 1992 FIRST-PERSON SHOOTER FROM ID SOFTWARE",
+      answer_text: "What is Wolfenstein 3D?"
+    },
+    50: {
+      question_text:
+        "THIS VIDEO GAME CHARACTER IS BLUE, COLLECTS RINGS, AND GOES FAST",
+      answer_text: "What is Sonic?"
+    }
+  }
+];
